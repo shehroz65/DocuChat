224310966345b691d6a3037927562ad4cb11835f
Shehroz Khan <shehrozkhan2000@gmail.com>
2024-04-10 17:21:25 +0500

    New files

--- Langchain.py
+++ Langchain.py
@@ -1,116 +1,109 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-# In[ ]:
-
-import os
-from langchain import HuggingFacePipeline
-from transformers import AutoTokenizer, pipeline
-import torch
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.document_loaders import OnlinePDFLoader, UnstructuredPDFLoader, PyPDFLoader
-from langchain.text_splitter import TokenTextSplitter
-from langchain.memory import ConversationBufferMemory
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 
+import time
 
-# In[ ]:
+# from langchain.chat_models import ChatOpenAI
+from transformers import AutoTokenizer, pipeline
+from auto_gptq import AutoGPTQForCausalLM
+from langchain.llms import HuggingFacePipeline
+from langchain import PromptTemplate, LLMChain
 
-
-
-
-# In[ ]:
-
-
-
-
-model = "tiiuae/falcon-7b-instruct" #tiiuae/falcon-40b-instruct
-
-tokenizer = AutoTokenizer.from_pretrained(model)
-
-pipeline = pipeline(
-    "text-generation", #task
-    model=model,
-    tokenizer=tokenizer,
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-    device_map="cuda",
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id
-)
+DOCUMETNS_DB_DIR = ''
 
 
-# In[ ]:
+class DocSearchWrapper:
+    def __init__(self):
+        db = Chroma(
+            persist_directory=DOCUMETNS_DB_DIR,
+            # embedding_function=OpenAIEmbeddings(),
+            embedding_function=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2"),
+        )
+
+        retriever = db.as_retriever(search_kwargs={"k": 3})
+
+        model_name_or_path = "TheBloke/Llama-2-13B-chat-GPTQ"
+        #model_basename = "gptq_model-4bit-128g"
+
+        use_triton = False
+
+        tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+
+        model = AutoGPTQForCausalLM.from_quantized(
+            model_name_or_path,
+            use_safetensors=True,
+            trust_remote_code=True,
+            device="cuda:0",
+            use_triton=use_triton,
+            quantize_config=None,
+        )
+
+        pipe = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=tokenizer,
+            max_new_tokens=4096,
+            temperature=0.8,
+            top_p=0.95,
+            repetition_penalty=1.15,
+        )
+
+        self.llm = HuggingFacePipeline(pipeline=pipe)
+
+        # Interactive questions and answers
+        self.CRChain = ConversationalRetrievalChain.from_llm(
+            # llm=ChatOpenAI(model_name="gpt-3.5-turbo"),
+            llm=self.llm,
+            chain_type="stuff",
+            retriever=retriever,
+            return_source_documents=True,
+            # condense_question_llm=self.llm
+            # condense_question_llm=ChatOpenAI(),
+        )
+
+        self.chat_history = []
+
+    def search_docbase(self, query):
+        result = self.CRChain({"question": query, "chat_history": self.chat_history})
+
+        self.chat_history.append((query, result["answer"]))
+
+        return result
+
+    def clear_history(self):
+        self.chat_history = []
 
 
-llm = HuggingFacePipeline(pipeline = pipeline, model_kwargs = {'temperature':0})
+if __name__ == "__main__":
+    doc_search = DocSearchWrapper()
 
+    while True:
+        query = input("\nEnter a query: ")
+        if query == "exit":
+            break
+        if query == "clear":
+            doc_search.clear_history()
+            continue
+        if query.strip() == "":
+            continue
 
-# In[ ]:
+        # Get the answer from the chain
+        start = time.time()
+        res = doc_search.search_docbase(query)
+        print(res)
 
+        answer, docs = res["answer"], res["source_documents"]
+        end = time.time()
 
-loader = PyPDFLoader("Q.pdf")
-pdfData = loader.load()
+        # Print the result
+        print("\n\n> Question:")
+        print(query)
+        print(f"\n> Answer (took {round(end - start, 2)} s.):")
+        print(answer)
 
-text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=0)
-splitData = text_splitter.split_documents(pdfData)
-print ("Split Data is", splitData)
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-collection_name = "clarett_collection"
-local_directory = "clarett_vect_embedding"
-persist_directory = os.path.join(os.getcwd(), local_directory)
-
-embeddings = HuggingFaceEmbeddings()
-vectDB = Chroma.from_documents(splitData,
-                      embeddings,
-                      collection_name=collection_name,
-                      persist_directory=persist_directory
-                      )
-vectDB.persist()
-
-
-# In[ ]:
-
-
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-chatQA = ConversationalRetrievalChain.from_llm(
-            llm,
-            vectDB.as_retriever(),
-            memory=memory)
-
-print ("Memory is", memory)
-print ("Chatqa is ", chatQA)
-# In[ ]:
-
-
-chat_history = []
-qry = ""
-while qry != 'done':
-    qry = input('Question: ')
-    if qry != exit:
-        print ("Gettting response")
-        response = chatQA({"question": qry, "chat_history": chat_history})
-        
-        print(response)
-        print ("GOT RESPONSE")
-
-
-# In[ ]:
-
-
-#
-
+        # Print the relevant sources used for the answer
+        print("Sources:\n")
+        for document in docs:
+            # print("> " + document.metadata["source"] + f": page({document.metadata['page']})")
+            print("> " + document.metadata["source"])
